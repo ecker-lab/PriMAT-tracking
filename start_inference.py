e14fc2e4b317c757cb7569a2044afa76df3a9f26
Matthias Nuske <matthias.nuske@uni-goettingen.de>
2022-08-11 17:22:19 +0200

    fixed inference for pose disabled mode

--- start_inference.py
+++ start_inference.py
@@ -3,13 +3,13 @@ import os
 os.chdir('/home/matthias/monkey/MC-FairMOT_mt/src')
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "1" 
+os.environ["CUDA_VISIBLE_DEVICES"] = "3" 
 from pathlib import Path
 p = Path('/media/hdd2/matthias/sfb1528_storage/Z01/PlaygroundSessions4ActionClassification/')
 q = Path('/media/hdd2/matthias/sfb1528_storage/Z01/Output_Pose/')
 import shutil
 
-for inp in p.glob('luk*.mp4'):
+for inp in p.glob('luk*69.mp4'):
 
     # get rid of .mp4
     # dir_in = p / inp.stem
@@ -30,6 +30,6 @@ for inp in p.glob('luk*.mp4'):
         # ou = Path(dir_out, inp2.name)
         # os.system("python demo.py mot --load_model ../models/mcFairmotPose.pth --conf_thres 0.2 --det_thres 0.7 --emb_sim_thres 0.1 --iou_sim_thres 0.1 --input_video "+str(inp2)+" --output_root "+str(ou))
     if len(os.listdir(dir_out)) == 0: # Check is empty..
-        os.system(f"python demo.py mot --load_model ../models/mcFairmotPose_without-val.pth --conf_thres 0.2 --det_thres 0.7 --emb_sim_thres 0.1 --iou_sim_thres 0.1 --input_video {str(inp)} --output_root {str(dir_out)} --output_format text")
+        os.system(f"python demo.py mot-p --load_model ../models/mcFairmotPose_without-val.pth --conf_thres 0.2 --det_thres 0.7 --emb_sim_thres 0.1 --iou_sim_thres 0.1 --input_video {str(inp)} --output_root {str(dir_out)} --output_format text --use_pose")
     else:
         print('not this one==============================')
