4545164b8c564eaa05f9d8c0f6c06eb641259059
Matthias Nuske <matthias.nuske@uni-goettingen.de>
2022-08-29 16:46:36 +0200

    update -- latest used

--- experiments/run_inference.sh
+++ experiments/run_inference.sh
@@ -1,7 +1,7 @@
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=0
 
 cd src
-python demo.py mot --load_model ../exp/mot/lemur_box/model_last.pth --conf_thres 0.02 --det_thres 0.5 --input_video /home/matthias/storage/sfb1528_storage/Z02/LemursAndBoxes/lemur_videos/Train25_close_1.mp4 --output_root /home/matthias/storage/sfb1528_storage/Z02/LemursAndBoxes/out/Train25_close_1 --store_opt
+python demo.py mot --load_model ../exp/mot/lab_all_pose_2ndTry/model_last.pth --conf_thres 0.02 --det_thres 0.5 --input_video /home/matthias/monkey/trim15.mp4 --output_root /home/matthias/monkey/richard_test-pose --store_opt --use_pose
 cd ..
 
--- experiments/run_train.sh
+++ experiments/run_train.sh
@@ -1,5 +1,5 @@
 cd src
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-python train.py mot --exp_id lemur_box --gpus 0,1,2,3 --batch_size 12 --load_model '../models/model_120.pth' --num_epochs 400 --lr_step '200' --data_cfg '../src/lib/cfg/lemur_box.json' #--use_pose #--trainval --val_intervals 10 --train_only_pose #--cat_spec_wh '../models/model_120.pth'
+export CUDA_VISIBLE_DEVICES=0
+python train.py mot --exp_id test_changes2 --gpus 0 --batch_size 1 --load_model '../models/model_120.pth' --num_epochs 40 --lr_step '20' --data_cfg '../src/lib/cfg/lab.json' --trainval --val_intervals 2 #--use_pose #--train_only_pose #--cat_spec_wh '../models/model_120.pth'
 cd ..
