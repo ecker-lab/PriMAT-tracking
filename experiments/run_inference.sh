c6db131991542d81bf63da2e44cded664f2afbe8
Matthias Nuske <matthias.nuske@uni-goettingen.de>
2022-12-19 09:36:06 +0100

    minor branch corrections

--- experiments/run_inference.sh
+++ experiments/run_inference.sh
@@ -1,7 +1,7 @@
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
 cd src
-python demo.py mot --load_model ../exp/mot/test_pose_high_batch-loss_fix2/model_last.pth --conf_thres 0.02 --det_thres 0.5 --input_video /home/matthias/monkey/trim15.mp4 --output_root /home/matthias/monkey/test_pose_high_batch-loss_fix2/ --store_opt --line_thickness 2 --use_pose #--reid_cls_names 'lemur,box' --reid_cls_ids '0,1' --cat_spec_wh #--use_pose
+python demo.py mot --load_model ../exp/mot/with_WH-lemur_box-7-11-22/model_last.pth --conf_thres 0.02 --det_thres 0.5 --input_video /home/matthias/monkey/lemur_box-trim15.mp4 --output_root /home/matthias/monkey/with_cat2/ --store_opt --line_thickness 2 --reid_cls_names 'lemur,box' --reid_cls_ids '0,1' --cat_spec_wh #--use_pose
 cd ..
 
--- experiments/run_train.sh
+++ experiments/run_train.sh
@@ -1,5 +1,5 @@
 cd src
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-python train.py mot --exp_id WH-Pose-17-10-22 --gpus 0,1,2,3 --batch_size 24 --load_model '../models/model_120.pth' --num_epochs 60 --lr_step '20,40' --data_cfg '../src/lib/cfg/explorationroom_largest.json' --use_pose --cat_spec_wh --store_opt #--reid_cls_names 'lemur,box' --reid_cls_ids '0,1' #--trainval --val_intervals 2 #--train_only_pose '../models/model_120.pth'
+export CUDA_VISIBLE_DEVICES=2,3
+python train.py mot --exp_id without_WH-lemur_box-7-11-22 --gpus 0,1 --batch_size 12 --load_model '../models/model_120.pth' --num_epochs 60 --lr_step '20,40' --data_cfg '../src/lib/cfg/lemur_box.json' --store_opt --reid_cls_names 'lemur,box' --reid_cls_ids '0,1' #--cat_spec_wh --use_pose --trainval --val_intervals 2 #--train_only_pose '../models/model_120.pth'
 cd ..
